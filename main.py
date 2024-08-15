7f8a5802b6d0cd46e1da1d945f85b8ebd9b81431
optimizt561 <emmanuelumoh996@gmail.com>
2024-08-15 04:32:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -300,8 +300,10 @@ def send_email(name, email, phone, message):
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
-        connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
-        connection.sendmail(email, MAIL_APP_PW, Messag)
+        connection.sendmail(from_addr=MAIL_ADDRESS,
+            to_addrs=hotmail,
+            msg=email_message)
+        # connection.sendmail(email, MAIL_APP_PW, Messag)
 
 
 if __name__ == "__main__":
